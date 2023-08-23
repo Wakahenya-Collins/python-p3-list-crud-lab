97de4a19bf35ad10058a035323530da822d58b3b
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-08-23 13:10:15 +0300

    Done with assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,59 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    new_list = [1, 2, 3, 4]  # Replace these elements with your desired elements
+    return new_list
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(lst, element):
+    lst.append(element)
+    return lst
 
-def add_element_to_start_of_list(l, element):
-    return None
 
-def remove_element_from_end_of_list(l):
-    return None
+def add_element_to_start_of_list(lst, element):
+    lst.insert(0, element)
+    return lst
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_end_of_list(lst):
+    lst.pop()
+    return lst
 
-def retrieve_first_element_from_list(l):
-    return None
+def remove_element_from_start_of_list(lst):
+    del lst[0]
+    return lst
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_first_element_from_list(input_list):
+    return input_list[0]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_element_from_index(input_list, index):
+    return input_list[index]
+
+def retrieve_last_element_from_list(input_list):
+    return input_list[-1]
+
+empty_list = create_an_empty_list()
+print(empty_list)
+
+my_list = create_a_list()
+print(my_list)
+
+add_element_to_end_of_list(my_list, 5)
+print(my_list)
+
+add_element_to_start_of_list(my_list, 0)
+print(my_list)
+
+remove_element_from_end_of_list(my_list)
+print(my_list)
+
+remove_element_from_start_of_list(my_list)
+print(my_list)
+
+first_element = retrieve_first_element_from_list(my_list)
+print(first_element)
+
+element_at_index = retrieve_element_from_index(my_list, 1)
+print(element_at_index)
+
+last_element = retrieve_last_element_from_list(my_list)
+print(last_element)
